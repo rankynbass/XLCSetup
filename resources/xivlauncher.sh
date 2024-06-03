db5efeeaaebbfdbb8cf8258d9f7ed0c480824724
Rankyn Bass <rankyn@proton.me>
2024-06-02 18:23:15 -0700

    Working python installer

--- resources/xivlauncher.sh
+++ resources/xivlauncher.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-if [ -z "$XDG_DATA_HOME" ]; then
-    XDG_DATA_HOME="$HOME/.local/share"
-fi
-xldir="XDG_DATA_HOME/NAME"
-export OPENSSL_CONF="$xldir/openssl_fix.cnf"
-
-"$xldir/XIVLauncher.Core"
+#!/bin/bash
+installdir=
+parentdir="$(dirname "$installdir")"
+"$installdir/install" --update --custom "$parentdir"
+export OPENSSL_CONF="$installdir/openssl_fix.cnf"
+"$installdir/XIVLauncher/XIVLauncher.Core" $@
