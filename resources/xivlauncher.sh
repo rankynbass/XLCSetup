929382a567b0e2c4043d0231cc50fe34d2700ab2
Rankyn Bass <rankyn@proton.me>
2024-06-05 17:57:22 -0700

    Rename things for easier reading * install file becomes xli * class xiv becomes xlcore_local_installer * instances of class become xli * local install to xivlauncher-core instead of -local

--- resources/xivlauncher.sh
+++ resources/xivlauncher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 installdir=
-"$installdir/install" --update
+"$installdir/xli" --update
 export OPENSSL_CONF="$installdir/openssl_fix.cnf"
 "$installdir/XIVLauncher/XIVLauncher.Core" $@
